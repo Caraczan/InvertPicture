562bf43ac26052a3e9a24c1208ee48139d328f13
Caraczan <przemek.banaszek@outlook.com>
2019-12-07 18:00:33 +0100

    addded install_required.sh, it will isntall needed things to for python script to work.

---
+++ install_required.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# installing homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# installing python3 from homebrew
+brew install python3
+# installing imagegick from homebrew
+brew install imagemagick
